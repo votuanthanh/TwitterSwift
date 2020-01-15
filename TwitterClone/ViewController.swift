8147819a28889c981d3198ebb28c61adf7c96e04
ThanhVT <thanh.dtu94@gmail.com>
2020-01-14 22:48:55 -0800

    Initial Commit

---
+++ TwitterClone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TwitterClone
+//
+//  Created by ThanhVt on 1/14/20.
+//  Copyright © 2020 ThanhVt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
